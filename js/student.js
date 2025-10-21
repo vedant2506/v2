f1553aecfe01bd615fa99ffb75e23aee2d9eafc1
vedant2506 <vedant.gawali06@gmail.com>
2025-10-21 11:12:14 +0530

    audio file route changes

--- js/student.js
+++ js/student.js
@@ -237,7 +237,7 @@ function loadStudent(db) {
                         'This device has already been used for this session. Attempt flagged.'
                     );
                 }
-                const audio = new Audio('../error.mp3'); // replace with your audio file path
+                const audio = new Audio('error.mp3'); // replace with your audio file path
                 audio.play();
                 showResult('This device has already been used for this session. Attempt flagged.', 'error', method);
             } else {
@@ -248,7 +248,7 @@ function loadStudent(db) {
                         'An error occurred. The session may be inactive or your roll number is invalid for this class.'
                     );
                 }
-                const audio = new Audio('../error.mp3'); // replace with your audio file path
+                const audio = new Audio('error.mp3'); // replace with your audio file path
                 audio.play();
                 showResult('An error occurred. The session may be inactive or your roll number is invalid for this class.', 'error', method);
             }
@@ -260,7 +260,7 @@ function loadStudent(db) {
                     'You are marked present.'
                 );
             }
-            const audio = new Audio('../notification.mp3'); // replace with your audio file path
+            const audio = new Audio('notification.mp3'); // replace with your audio file path
             audio.play();
             showResult(`Success, Roll No: ${loggedInRollNo}! You are marked present.`, 'success', method);
         }
